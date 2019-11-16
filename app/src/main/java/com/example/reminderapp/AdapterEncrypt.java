d9bbea94e5b3ee37845d73bd94ad5979324ab7e7
Saurabhnh30 <saurabh.kumar.skp121@gmail.com>
2019-11-16 18:09:49 +0530

    encrypt doene

--- app/src/main/java/com/example/reminderapp/AdapterEncrypt.java
+++ app/src/main/java/com/example/reminderapp/AdapterEncrypt.java
@@ -1,5 +1,50 @@
 package com.example.reminderapp;
 
-public class AdapterEncrypt
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AdapterEncrypt extends RecyclerView.Adapter<AdapterEncrypt.EncryViewHolder>
 {
+
+    Context context;
+
+
+    public AdapterEncrypt(Context c) {
+        this.context = c;
+    }
+
+    @NonNull
+    @Override
+    public EncryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new EncryViewHolder(LayoutInflater.from(this.context).inflate(R.layout.encryptlayout, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EncryViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class EncryViewHolder extends RecyclerView.ViewHolder
+    {
+
+        TextView encrytitle, encrydata;
+
+        public EncryViewHolder(@NonNull View itemView)
+        {
+            super(itemView);
+            encrytitle = itemView.findViewById(R.id.encrypttitle);
+            encrydata =  itemView.findViewById(R.id.encrydata);
+        }
+    }
 }
--- app/src/main/java/com/example/reminderapp/EncryptScreen.java
+++ app/src/main/java/com/example/reminderapp/EncryptScreen.java
@@ -2,29 +2,20 @@ package com.example.reminderapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.io.UnsupportedEncodingException;
-import java.math.BigInteger;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -32,18 +23,11 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
 
-public class encry extends AppCompatActivity
+public class EncryptScreen extends AppCompatActivity
 {
     FirebaseFirestore firestore;
 
-    EditText encrypttitle;
-    TextView decrypttitle;
-    Button encryptbutton, decryptbutton;
-    List<Byte> ed = new ArrayList<Byte>();
-
-    private String publickey = " " ;
-    private String privatekey = " " ;
-    private byte[] encodeData = null;
+    RecyclerView recyclerView;
 
     private byte encryptionKey[] = {9, 115, 51, 86, 105, 4, -31, -23, -68, 88, 17, 20, 3, -105, 119, -53};
     private Cipher cipher, decipher;
@@ -53,14 +37,15 @@ public class encry extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_encry);
+        setContentView(R.layout.activity_encrypt_screen);
 
         firestore = FirebaseFirestore.getInstance();
 
-        encrypttitle = findViewById(R.id.encrypttitle);
-        encryptbutton = findViewById(R.id.encryptbutton);
-        decrypttitle = findViewById(R.id.decrypttitle);
-        decryptbutton = findViewById(R.id.decryptbutton);
+        recyclerView = findViewById(R.id.encryptview);
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(EncryptScreen.this));
+
 
         try {
             try {
@@ -80,47 +65,11 @@ public class encry extends AppCompatActivity
     }
 
 
-
-    public void encrypt(View view)
-    {
-        String s = AESEncryptionMethod(encrypttitle.getText().toString());
-        Log.d("MAIN_ACT_LOG", "encrypted data : " + s);
-
-        encrypttitle.setText("");
-
-        HashMap<String, Object> data = new HashMap<>();
-        data.put("title", s);
-
-        firestore.collection("encrypt").add(data)
-                .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
-                    @Override
-                    public void onComplete(@NonNull Task<DocumentReference> task) {
-                        Toast.makeText(
-                                encry.this,
-                                "Saved to firestore",
-                                Toast.LENGTH_LONG
-                        ).show();
-                    }
-                });
-
-    }
-
-    public void deceypt(View view)
-    {
-
-        firestore.collection("encrypt").limit(1).get()
+    public void getAllData() {
+        firestore.collection("encrypt").get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        String encryptData = task.getResult().getDocuments().get(0).getString("title");
-                        Log.d("ENCRYPT_LOG", encryptData);
-
-                        try {
-                            decrypttitle.setText(AESDecryptionMethod(encryptData));
-                        }
-                        catch (Exception e) {
-                            e.printStackTrace();
-                        }
 
                     }
                 });
