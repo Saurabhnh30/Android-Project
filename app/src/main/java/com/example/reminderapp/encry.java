0c941212144fb75af1e38b8b7073c84431701849
Saurabhnh30 <saurabh.kumar.skp121@gmail.com>
2019-11-16 15:41:18 +0530

    encrypt partial

--- app/src/main/java/com/example/reminderapp/encry.java
+++ app/src/main/java/com/example/reminderapp/encry.java
@@ -17,9 +17,20 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
 
 public class encry extends AppCompatActivity
 {
@@ -28,10 +39,15 @@ public class encry extends AppCompatActivity
     EditText encrypttitle;
     TextView decrypttitle;
     Button encryptbutton, decryptbutton;
+    List<Byte> ed = new ArrayList<Byte>();
 
     private String publickey = " " ;
     private String privatekey = " " ;
     private byte[] encodeData = null;
+
+    private byte encryptionKey[] = {9, 115, 51, 86, 105, 4, -31, -23, -68, 88, 17, 20, 3, -105, 119, -53};
+    private Cipher cipher, decipher;
+    private SecretKeySpec secretKeySpec;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -47,15 +63,32 @@ public class encry extends AppCompatActivity
         decryptbutton = findViewById(R.id.decryptbutton);
 
 
-        try
-        {
-            Map<String , Object> keymap = rsa.initKey();
-            publickey =  rsa.getPublicKey(keymap);
-            privatekey = rsa.getPrivateKey(keymap);
+//        try
+//        {
+//            Map<String , Object> keymap = rsa.initKey();
+//            publickey =  rsa.getPublicKey(keymap);
+//            privatekey = rsa.getPrivateKey(keymap);
+//
+//        }
+//        catch (Exception e)
+//        {
+//            e.printStackTrace();
+//        }
 
+
+        try {
+            try {
+                cipher = Cipher.getInstance("AES");
+                decipher = Cipher.getInstance("AES");
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            } catch (NoSuchPaddingException e) {
+                e.printStackTrace();
+            }
+
+            secretKeySpec = new SecretKeySpec(encryptionKey, "AES");
         }
-        catch (Exception e)
-        {
+        catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -65,32 +98,50 @@ public class encry extends AppCompatActivity
     public void encrypt(View view)
     {
 
-        byte[] rsaData = encrypttitle.getText().toString().getBytes();
+//        byte[] rsaData = encrypttitle.getText().toString().getBytes();
+//
+//        try {
+//            encodeData = rsa.encryptByPublicKey(rsaData,publickey);
+//            String encodeStr = new BigInteger(1, encodeData).toString();
+//
+//            HashMap<String, Object> data = new HashMap<>();
+//            data.put("title", encodeStr);
+//
+//            firestore.collection("encrypt").add(data)
+//                    .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+//                        @Override
+//                        public void onComplete(@NonNull Task<DocumentReference> task) {
+//                            Toast.makeText(
+//                                    encry.this,
+//                                    "Saved to firestore",
+//                                    Toast.LENGTH_LONG
+//                            ).show();
+//                        }
+//                    });
+//        }
+//        catch (Exception e) {
+//            e.printStackTrace();
+//        }
 
-        try {
-            encodeData = rsa.encryptByPublicKey(rsaData,publickey);
-            String encodeStr = new BigInteger(1, encodeData).toString();
+        String s = AESEncryptionMethod(encrypttitle.getText().toString());
+        Log.d("MAIN_ACT_LOG", "encrypted data : " + s);
+//        databaseReference.child(Long.toString(date.getTime())).setValue(s);
+        encrypttitle.setText("");
 
-            HashMap<String, Object> data = new HashMap<>();
-            data.put("title", encodeStr);
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("title", s);
 
-            firestore.collection("encrypt").add(data)
-                    .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
-                        @Override
-                        public void onComplete(@NonNull Task<DocumentReference> task) {
-                            Toast.makeText(
-                                    encry.this,
-                                    "Saved to firestore",
-                                    Toast.LENGTH_LONG
-                            ).show();
-                        }
-                    });
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-
-
+        firestore.collection("encrypt").add(data)
+                .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentReference> task) {
+                        Toast.makeText(
+                                encry.this,
+                                "Saved to firestore",
+                                Toast.LENGTH_LONG
+                        ).show();
+                    }
+                });
 
     }
 
@@ -101,20 +152,65 @@ public class encry extends AppCompatActivity
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        Log.d("ENCRYPT_LOG", task.getResult().getDocuments().get(0).getString("title"));
-
-                        byte[] ed = task.getResult().getDocuments().get(0).getString("title").getBytes();
+                        String encryptData = task.getResult().getDocuments().get(0).getString("title");
+                        Log.d("ENCRYPT_LOG", encryptData);
 
                         try {
-                            byte[] decodeData = rsa.decryptByPrivateKey(ed,privatekey);
-                            String decodeStr = new String(decodeData);
-                            decrypttitle.setText(decodeStr);
+                            decrypttitle.setText(AESDecryptionMethod(encryptData));
                         }
                         catch (Exception e) {
                             e.printStackTrace();
                         }
+
                     }
                 });
 
+    }
+
+
+    private String AESEncryptionMethod(String string){
+
+        byte[] stringByte = string.getBytes();
+        byte[] encryptedByte = new byte[stringByte.length];
+
+        try {
+            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+            encryptedByte = cipher.doFinal(stringByte);
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        }
+
+        String returnString = null;
+
+        try {
+            returnString = new String(encryptedByte, "ISO-8859-1");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return returnString;
+    }
+
+    private String AESDecryptionMethod(String string) throws UnsupportedEncodingException {
+        byte[] EncryptedByte = string.getBytes("ISO-8859-1");
+        String decryptedString = string;
+
+        byte[] decryption;
+
+        try {
+            decipher.init(cipher.DECRYPT_MODE, secretKeySpec);
+            decryption = decipher.doFinal(EncryptedByte);
+            decryptedString = new String(decryption);
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        }
+        return decryptedString;
     }
 }
